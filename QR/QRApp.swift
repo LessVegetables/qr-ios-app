e2bd2dcf3bd3a9b36022900b485d7297403b6736
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2026-02-19 22:23:41 +0700

    Initial Commit

---
+++ QR/QRApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRApp.swift
+//  QR
+//
+//  Created by Daniel Gehrman on 19.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct QRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
