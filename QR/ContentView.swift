f831045dacd62b26689dad8d498022138c6f84f3
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2026-03-01 16:45:15 +0700

    Changes to be committed: 	modified:   QR.xcodeproj/project.pbxproj 	new file:   QR.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved 	modified:   QR/ContentView.swift

--- QR/ContentView.swift
+++ QR/ContentView.swift
@@ -6,16 +6,47 @@
 //
 
 import SwiftUI
+import QRCode
 
 struct ContentView: View {
+    
+    @State var qr_code_text = "content"
+    
+    private var qrCGImage: CGImage? {
+        do {
+            let doc = try QRCode.Document(utf8String: qr_code_text)
+            return try doc.cgImage(dimension: 400)
+        } catch {
+            print("QR generation failed:", error)
+            return nil
+        }
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            if let qrCGImage {
+                Image(decorative: qrCGImage, scale: 1)
+                    .interpolation(.none)   // keeps it crisp
+                    .resizable()
+                    .frame(width: 200, height: 200)
+            } else {
+                Text("Couldn’t generate QR code")
+                    .foregroundStyle(.secondary)
+            }
+
+            TextField(
+                "Input",
+                text: $qr_code_text
+            )
+            .disableAutocorrection(true)
+            .textFieldStyle(.roundedBorder)
+//            
+//            Button(action: copyImageToClipboard) {
+//                Text("Sign In")
+//            }
         }
         .padding()
+        
     }
 }
 
